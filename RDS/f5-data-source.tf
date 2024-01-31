157b117d4460cbabeea51ec518739dcbcb3b1038
Umar <dvop.omer@gmail.com>
2024-02-01 00:39:09 +0530

    rds update

--- RDS/f5-data-source.tf
+++ RDS/f5-data-source.tf
@@ -1,10 +1,10 @@
 data "aws_db_instance" "rds_mysql" {
-  identifier          = "mysql-db"
-  instance_class      = "db.t2.micro"
-  engine              = "mysql"
-  engine_version      = "5.7"
-  storage_type        = "gp2"
-  allocated_storage   = 20
-  publicly_accessible = false
+  db_instance_identifier = "my-rds-instance"
+  instance_class         = "db.t2.micro"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  storage_type           = "gp2"
+  allocated_storage      = 20
+  publicly_accessible    = false
 }
 
