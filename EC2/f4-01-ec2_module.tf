051230451d91dafce9432d04f13e692ffd21703f
Umar <dvop.omer@gmail.com>
2024-01-31 21:37:18 +0530

    ec2  module

---
+++ EC2/f4-01-ec2_module.tf
@@ -0,0 +1,10 @@
+module "ec2-instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.6.0"
+
+  ami               = data.aws_ami.ubuntu
+  instance_type     = t2-micro
+  availability_zone = "us-east-1a"
+
+
+}
--- aws-1.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1" # Change this to your desired AWS region
-}
-
-resource "aws_vpc" "example" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-}
